4a34366f3a770c30bd544688a8dc5c9929dd6c57
Robert Pop <poprobertdaniel22@gmail.com>
2018-11-21 17:48:46 +0200

    updates

--- chunks/intro/components/FeaturedNews.js
+++ chunks/intro/components/FeaturedNews.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Row, Col } from 'antd';
-import { FeaturedRank, FeaturedTitle, PostDate } from './StyledComponents';
+import React from 'react'
+import { Row, Col } from 'antd'
+import { FeaturedRank, FeaturedTitle, PostDate } from './StyledComponents'
 
 class FeaturedNews extends React.PureComponent {
   renderNewsCard(post) {
-    const { title, rank, date } = post;
+    const { title, rank, date } = post
     return (
       <Row
         style={{
@@ -17,22 +17,22 @@ class FeaturedNews extends React.PureComponent {
         <Col span={4}>
           <FeaturedRank>{rank}</FeaturedRank>
         </Col>
-        <Col span={19} offset={1} style={{ height: '120px' }}>
+        <Col span={19} offset={1} style={{ height: '150px' }}>
           <FeaturedTitle>{title}</FeaturedTitle>
           <PostDate>{date}</PostDate>
         </Col>
       </Row>
-    );
+    )
   }
 
   render() {
-    const { news } = this.props;
+    const { news } = this.props
     return (
       <div style={{ marginTop: '20px' }}>
         {news.map(post => this.renderNewsCard(post))}
       </div>
-    );
+    )
   }
 }
 
-export default FeaturedNews;
+export default FeaturedNews
