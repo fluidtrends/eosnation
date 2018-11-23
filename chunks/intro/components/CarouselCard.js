83e393671626dd6fc407ee1078476740f379d157
Robert Pop <poprobertdaniel22@gmail.com>
2018-11-23 14:41:14 +0200

    navigation updates

--- chunks/intro/components/CarouselCard.js
+++ chunks/intro/components/CarouselCard.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React from 'react'
 
 class CarouselCard extends React.Component {
-    render() {
-        const {image, title, date, btnLink, btnText} = this.props;
-        console.log("date is:", date)
-        return (
-            <div className='carousel-card-container'>
-                <div className='carousel-card-img'><img src={image}/></div>
-                <div className="carousel-card-info flex-col-space-between">
-                    <div className='paragraph'>{title}</div>
-                     <div className='flex-row-space-between'>
-                        <p className='section-text'>{date}</p>
-                        <p className='section-text'><a href={btnLink}>{btnText}</a></p>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const { image, title, date, btnLink, btnText } = this.props
+
+    return (
+      <div className="carousel-card-container">
+        <div className="carousel-card-img">
+          <img src={image} />
+        </div>
+        <div className="carousel-card-info flex-col-space-between">
+          <div className="paragraph">{title}</div>
+          <div className="flex-row-space-between">
+            <p className="section-text">{date}</p>
+            <p className="section-text">
+              <a href={btnLink}>{btnText}</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default CarouselCard;
+export default CarouselCard
