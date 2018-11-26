2cc2604a137b56aa7ae857b4197f04542ab80707
Robert Pop <poprobertdaniel22@gmail.com>
2018-11-26 20:30:37 +0200

    why page

---
+++ assets/text/why.md
@@ -0,0 +1,7 @@
+## How has EOS Nation demonstrated its ability to generate value for the EOS ecosystem?
+
+### EOS Nation's mission is to add value to the EOS ecosystem by focusing our efforts on education through community engagement. EOS Nation has been one of the most active Block Producer candidates, establishing a strong presence online in the various public EOS chats (most notably, in the Governance and Infrastructure channels) as well as across many cities around the world.Here are some examples that demonstrate EOS Nation’s capacity to generate value for the network, which translates into value for all EOS token holders across the world.
+
+## EOS Nation Meetups
+
+### In the last few weeks, EOS Nation has hosted more than twenty EOS Nation Meetups in various countries including Canada, China, Nigeria, Ukraine, Kosovo, Switzerland and the USA. Our meetups vary in style and size, but all of them allow us to spread some EOS knowledge and build new relationships, which leads to a healthy and growing EOS community.
--- chunks/intro/components/Cover.js
+++ chunks/intro/components/Cover.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Component, Components } from 'react-dom-chunky'
 import { Button } from 'rmwc/Button'
 import { Typography } from '@rmwc/typography'
-import { Cover, CoverContent, SocialLinks } from './StyledComponents';
+import { Cover, CoverContent, SocialLinks } from './StyledComponents'
 
-import 'antd/dist/antd.css';
+import 'antd/dist/antd.css'
 
 export default class Intro extends Component {
   constructor(props) {
@@ -19,7 +19,6 @@ export default class Intro extends Component {
 
   componentDidMount() {
     super.componentDidMount()
-
   }
 
   main() {
@@ -27,36 +26,50 @@ export default class Intro extends Component {
       <React.Fragment>
         <CoverContent>
           {this.title()}
-          {this.subtitle()}
+          {!this.props.hideSubtitle && this.subtitle()}
           {this.icons()}
         </CoverContent>
         <Cover backgroundImgURL={this.props.backgroundImgURL} />
       </React.Fragment>
-    );
+    )
   }
 
   title() {
     return (
-      <div style={{ maxWidth: "652px" }}>
-        <Typography use='headline4' className={'title'}>
+      <div style={{ maxWidth: '652px' }}>
+        <Typography use="headline4" className={'title'}>
           BUILDING OUR FUTURE ONE BLOCK AT A TIME
         </Typography>
       </div>
-    );
+    )
   }
 
   subtitle() {
     return (
       <div style={{ marginTop: '30px' }}>
-        <Button raised style={{
-          background: '#D66C44', cursor: 'pointer', height: 60, width: 196, marginRight: 20,
-          marginTop: '20px'
-        }}>
+        <Button
+          raised
+          style={{
+            background: '#D66C44',
+            cursor: 'pointer',
+            height: 60,
+            width: 196,
+            marginRight: 20,
+            marginTop: '20px'
+          }}
+        >
           Learn More
         </Button>
-        <Button raised style={{
-          background: '#2c4858', cursor: 'pointer', height: 60, width: 196, marginTop: "20px"
-        }}>
+        <Button
+          raised
+          style={{
+            background: '#2c4858',
+            cursor: 'pointer',
+            height: 60,
+            width: 196,
+            marginTop: '20px'
+          }}
+        >
           Vote
         </Button>
       </div>
@@ -64,20 +77,24 @@ export default class Intro extends Component {
   }
 
   icons() {
-    const socialMediaLinks = this.props.footer.socialMediaLinks;
-    return (<div style={{ position: 'relative' }}>
-      <SocialLinks>
-        <Components.SocialIcons
-          iconColor={'#ffffff'} vertical iconColorHover={'#2c4858'} size={24}
-          isSmallScreen={this.props.isSmallScreen} socialMediaLinks={socialMediaLinks}
-        />
-      </SocialLinks>
-    </div>);
+    const socialMediaLinks = this.props.footer.socialMediaLinks
+    return (
+      <div style={{ position: 'relative' }}>
+        <SocialLinks>
+          <Components.SocialIcons
+            iconColor={'#ffffff'}
+            vertical
+            iconColorHover={'#2c4858'}
+            size={24}
+            isSmallScreen={this.props.isSmallScreen}
+            socialMediaLinks={socialMediaLinks}
+          />
+        </SocialLinks>
+      </div>
+    )
   }
 
   render() {
-    return [
-      ...this.main()
-    ]
+    return [...this.main()]
   }
 }
