dd665c11441ed7c992a006c999345606d4230591
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-05 10:26:57 +0200

    trans

--- chunks/intro/components/ValueSection.js
+++ chunks/intro/components/ValueSection.js
@@ -2,21 +2,36 @@ import React from 'react'
 import { Component } from 'react-dom-chunky'
 import * as StyledComponents from './StyledComponents'
 import VerticalCard from './VerticalCard'
+import { Data } from 'react-chunky'
 
 class ValueSection extends Component {
   constructor(props) {
     super(props)
     this._done = this.done.bind(this)
-    this.state = { ...super.state }
+    this.state = { ...super.state, selectedLanguage: null, strings: null }
   }
 
   componentDidMount() {
     super.componentDidMount()
+    Data.Cache.retrieveCachedItem('selectedLanguage')
+      .then(selectedLanguage => {
+        this.setState({ selectedLanguage })
+      })
+      .catch(() => {
+        return
+      })
+    fetch(this.props.theme.translatedStrings)
+      .then(response => response.json())
+      .then(translatedTexts => {
+        this.setState({ strings: translatedTexts['welcome']['pillars'] })
+      })
+      .catch(() => '')
   }
 
   done() {}
 
   renderComponent() {
+    console.log(this.state)
     const { values, title, theme, imgPath, imgTitle } = this.props
     return (
       <StyledComponents.ValuesSection>
