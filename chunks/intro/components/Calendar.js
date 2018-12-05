85e03e5cba8dd0d360226d81631c54b75995ca22
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-05 23:42:16 +0200

    trnslation

--- chunks/intro/components/Calendar.js
+++ chunks/intro/components/Calendar.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Grid, GridCell, GridInner } from 'rmwc';
-import '@material/layout-grid/dist/mdc.layout-grid.css';
+import React, { Component } from 'react'
+import { Grid, GridCell, GridInner } from 'rmwc'
+import '@material/layout-grid/dist/mdc.layout-grid.css'
 import {
   Body2,
   CalenderItemContent,
@@ -9,51 +9,85 @@ import {
   Heading3,
   Heading4,
   Icon
-} from './StyledComponents';
+} from './StyledComponents'
 
 export class CalendarItem extends Component {
   render() {
-    const { slug, name, location, date, timeInterval, extraInfoLink, facebookLink, twitterLink, meetupLink } = this.props;
-    let metaInfo = [];
+    const {
+      slug,
+      name,
+      location,
+      date,
+      timeInterval,
+      extraInfoLink,
+      facebookLink,
+      twitterLink,
+      meetupLink,
+      btnText
+    } = this.props
+    let metaInfo = []
 
     if (location) {
-      metaInfo.push(<Body2><i className="fas fa-map-marker margin-right-small" />{location}</Body2>);
+      metaInfo.push(
+        <Body2>
+          <i className="fas fa-map-marker margin-right-small" />
+          {location}
+        </Body2>
+      )
     }
     if (timeInterval) {
-      metaInfo.push(<Body2>
-        <i className="far fa-clock margin-right-small" />{timeInterval}
-      </Body2>);
+      metaInfo.push(
+        <Body2>
+          <i className="far fa-clock margin-right-small" />
+          {timeInterval}
+        </Body2>
+      )
     }
     if (extraInfoLink) {
-      metaInfo.push(<a href={extraInfoLink} target="_blank"><Body2 link>
-        <i className="fas fa-info-circle margin-right-small" />More info
-      </Body2></a>);
+      metaInfo.push(
+        <a href={extraInfoLink} target="_blank">
+          <Body2 link>
+            <i className="fas fa-info-circle margin-right-small" />
+            {btnText}
+          </Body2>
+        </a>
+      )
     }
 
     return (
       <CalenderItemWrapper>
         <CalenderItemDate>
-          <div style={{
-            fontSize: '86px',
-            textAlign: 'center',
-            textShadow: "0 2px 4px rgba(0, 0, 0, 0.5)"
-          }}>
+          <div
+            style={{
+              fontSize: '86px',
+              textAlign: 'center',
+              textShadow: '0 2px 4px rgba(0, 0, 0, 0.5)'
+            }}
+          >
             {date.getDate()}
           </div>
-          <div style={{
-            fontSize: '24px',
-            textAlign: 'center',
-            textShadow: "0 2px 4px rgba(0, 0, 0, 0.5)"
-          }}>
-            {`${new Intl.DateTimeFormat("en-US", { month: "short" }).format(date)} ${date.getFullYear()}`}
+          <div
+            style={{
+              fontSize: '24px',
+              textAlign: 'center',
+              textShadow: '0 2px 4px rgba(0, 0, 0, 0.5)'
+            }}
+          >
+            {`${new Intl.DateTimeFormat('en-US', { month: 'short' }).format(
+              date
+            )} ${date.getFullYear()}`}
           </div>
         </CalenderItemDate>
         <CalenderItemContent>
           <Grid className="padding-remove">
             <GridCell span="12">
               <GridInner>
-                <GridCell span="2"><Heading3>{slug}</Heading3></GridCell>
-                <GridCell span="10"><Heading4>{name}</Heading4></GridCell>
+                <GridCell span="2">
+                  <Heading3>{slug}</Heading3>
+                </GridCell>
+                <GridCell span="10">
+                  <Heading4>{name}</Heading4>
+                </GridCell>
               </GridInner>
             </GridCell>
             <GridCell span="12">
@@ -63,50 +97,93 @@ export class CalendarItem extends Component {
                 </GridCell>
                 <GridCell desktop="7" phone="2">
                   <GridInner>
-                    {metaInfo.length > 1 ? metaInfo.map((info, index) => (
-                      index !== 0 ? <GridCell span="4">{metaInfo[index]}</GridCell> : null
-                    )) : <Body2>-</Body2>}
+                    {metaInfo.length > 1 ? (
+                      metaInfo.map((info, index) =>
+                        index !== 0 ? (
+                          <GridCell span="4">{metaInfo[index]}</GridCell>
+                        ) : null
+                      )
+                    ) : (
+                      <Body2>-</Body2>
+                    )}
                   </GridInner>
                 </GridCell>
-                <GridCell span="3" phone="6" className="text-align-right calendar-social-links"
-                          style={{ marginLeft: "-20px" }}>
-                  {facebookLink ?
-                    <span><a className="margin-left-medium" href={facebookLink} target="_blank">
-                      <Icon className="fab fa-facebook-f facebook-custom-icon" /></a></span> : null
-                  }
-                  {twitterLink ?
-                    <span><a className="margin-left-medium" href={twitterLink} target="_blank"><Icon
-                      className="fab fa-twitter" /></a></span> : null
-                  }
-                  {meetupLink ?
-                    <span><a className="margin-left-medium" href={meetupLink} target="_blank"><Icon
-                      className="fab fa-meetup" /></a></span> : null
-                  }
+                <GridCell
+                  span="3"
+                  phone="6"
+                  className="text-align-right calendar-social-links"
+                  style={{ marginLeft: '-20px' }}
+                >
+                  {facebookLink ? (
+                    <span>
+                      <a
+                        className="margin-left-medium"
+                        href={facebookLink}
+                        target="_blank"
+                      >
+                        <Icon className="fab fa-facebook-f facebook-custom-icon" />
+                      </a>
+                    </span>
+                  ) : null}
+                  {twitterLink ? (
+                    <span>
+                      <a
+                        className="margin-left-medium"
+                        href={twitterLink}
+                        target="_blank"
+                      >
+                        <Icon className="fab fa-twitter" />
+                      </a>
+                    </span>
+                  ) : null}
+                  {meetupLink ? (
+                    <span>
+                      <a
+                        className="margin-left-medium"
+                        href={meetupLink}
+                        target="_blank"
+                      >
+                        <Icon className="fab fa-meetup" />
+                      </a>
+                    </span>
+                  ) : null}
                 </GridCell>
               </GridInner>
             </GridCell>
           </Grid>
         </CalenderItemContent>
       </CalenderItemWrapper>
-    );
+    )
   }
 }
 
 class Calendar extends Component {
   render() {
-    const { calendarEvents, theme, backgroundColor1, backgroundColor2, backgroundColor3 } = this.props;
+    const {
+      calendarEvents,
+      theme,
+      backgroundColor1,
+      backgroundColor2,
+      backgroundColor3,
+      btnText
+    } = this.props
     return (
-      <div className="align-center" style={{
-        maxWidth: '1120px',
-        background: `linear-gradient(0deg, ${backgroundColor1 || theme.secondaryColor} 0%,
-         ${backgroundColor2} 44.82%, ${backgroundColor3 || theme.primaryColor} 100%)`
-      }}>
-        {calendarEvents.map((event) => (
-          <CalendarItem {...event} />
+      <div
+        className="align-center"
+        style={{
+          maxWidth: '1120px',
+          background: `linear-gradient(0deg, ${backgroundColor1 ||
+            theme.secondaryColor} 0%,
+         ${backgroundColor2} 44.82%, ${backgroundColor3 ||
+            theme.primaryColor} 100%)`
+        }}
+      >
+        {calendarEvents.map(event => (
+          <CalendarItem {...event} btnText={btnText} />
         ))}
       </div>
-    );
+    )
   }
 }
 
-export default Calendar;
+export default Calendar
--- chunks/intro/components/EventsSection.js
+++ chunks/intro/components/EventsSection.js
@@ -1,26 +1,66 @@
 import React from 'react'
 import { Component } from 'react-dom-chunky'
-import Calendar from './Calendar';
-import { ValuesSection } from "./StyledComponents";
+import Calendar from './Calendar'
+import { ValuesSection } from './StyledComponents'
 
 class EventsSection extends Component {
   renderComponent() {
-    const { title, calendarEvents, action, subtitle } = this.props;
-    const events = calendarEvents.map(({ date, ...remainingProps }) =>
-      ({ date: new Date(date), ...remainingProps }));
+    const {
+      title,
+      calendarEvents,
+      action,
+      subtitle,
+      translation,
+      strings,
+      selectedLanguage,
+      btnText
+    } = this.props
+    const translatedTitle =
+      translation && strings && selectedLanguage
+        ? strings[selectedLanguage]['events'][`title`]
+        : title
+    const translatedSubtitle =
+      translation && strings && selectedLanguage
+        ? strings[selectedLanguage]['events'][`subtitle`]
+        : subtitle
+    const translatedBtnText =
+      translation && strings && selectedLanguage
+        ? strings[selectedLanguage]['events'][`btnText`]
+        : btnText
+    const events = calendarEvents.map(
+      ({ date, name, ...remainingProps }, index) => ({
+        date: new Date(date),
+        name:
+          translation && strings && selectedLanguage
+            ? strings[selectedLanguage]['events']['calendarEvents'][
+                `name${index}`
+              ]
+            : name,
+        ...remainingProps
+      })
+    )
+
     return (
       <ValuesSection>
         <h2 className="section-header text-align-center margin-bottom-large">
-          {title}
+          {translatedTitle}
         </h2>
         <h3 className="text-align-center  margin-bottom-large">
-          <a href={action} target="_blank" className="paragraph text-align-center">
-            {subtitle}
+          <a
+            href={action}
+            target="_blank"
+            className="paragraph text-align-center"
+          >
+            {translatedSubtitle}
           </a>
         </h3>
-        <Calendar {...this.props} calendarEvents={events}/>
+        <Calendar
+          {...this.props}
+          calendarEvents={events}
+          btnText={translatedBtnText}
+        />
       </ValuesSection>
-    );
+    )
   }
 }
 
