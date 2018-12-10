47d8acd694b3db364c2164ffc92b382bd6c5215d
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-10 22:38:39 +0200

    why translate

--- chunks/intro/screens/why.web.js
+++ chunks/intro/screens/why.web.js
@@ -1,15 +1,29 @@
 import React from 'react'
-import { Screen, Components } from 'react-dom-chunky'
+import { Screen } from 'react-dom-chunky'
 import * as CustomComponents from '../components'
+import { Data } from 'react-chunky'
 
 export default class WhySection extends Screen {
   constructor(props) {
     super(props)
-    this.state = { ...this.state }
+    this.state = { ...this.state, selectedLanguage: null, strings: null }
   }
 
   componentDidMount() {
     super.componentDidMount()
+    Data.Cache.retrieveCachedItem('selectedLanguage')
+      .then(selectedLanguage => {
+        this.setState({ selectedLanguage })
+      })
+      .catch(() => {
+        return
+      })
+    fetch(this.props.theme.translatedStrings)
+      .then(response => response.json())
+      .then(translatedTexts => {
+        this.setState({ strings: translatedTexts['why'] })
+      })
+      .catch(() => '')
   }
 
   loadCustomComponent(props) {
