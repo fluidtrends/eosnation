bdf67d3606cfca076d83b4175808f7465aaa989a
Robert Pop <poprobertdaniel22@gmail.com>
2019-01-09 17:05:56 +0200

    working version

--- chunks/intro/index.desktop.js
+++ chunks/intro/index.desktop.js
@@ -1,5 +1,6 @@
-import config from './chunk.json'
-import * as screens from './screens/index.desktop.js'
+// AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
 
+import config from "./chunk.json"
+import * as screens from "./screens/index.desktop"
 const chunk = { screens, ...config }
 export default chunk
--- chunks/intro/index.js
+++ chunks/intro/index.js
@@ -1,5 +1,6 @@
-import config from './chunk.json'
-import * as screens from './screens/index.js'
+// AUTO-GENERATED FILE. PLEASE DO NOT MODIFY. CHUNKY WILL CRY.
 
+import config from "./chunk.json"
+import * as screens from "./screens/index"
 const chunk = { screens, ...config }
 export default chunk
