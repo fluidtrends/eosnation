2819470dc78fa90dcf25ca63879922f62dc00bfd
Denis <carriere.denis@gmail.com>
2019-01-17 10:05:27 -0500

    fix bpjson

--- scripts/bpjson.sh
+++ scripts/bpjson.sh
@@ -6,4 +6,4 @@ cp -f bp.json .chunky/web/bp.aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeae
 
 # BOS
 cp -f bos.json .chunky/web/bos.json
-cp -f bos.json .chunky/web/bos.d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86.json
+cp -f bos.json .chunky/web/bp.d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86.json
