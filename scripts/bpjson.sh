04f47339e5f26c948898043e6b47f9cec6f73af4
Denis <carriere.denis@gmail.com>
2019-01-17 10:04:14 -0500

    Add chainID to bp.json

---
+++ scripts/bpjson.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# EOS
+cp -f bp.json .chunky/web/bp.json
+cp -f bp.json .chunky/web/bp.aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906.json
+
+# BOS
+cp -f bos.json .chunky/web/bos.json
+cp -f bos.json .chunky/web/bos.d5a3d18fbb3c084e3b1f3fa98c21014b5f3db536cc15d08f9f6479517c6a3d86.json
